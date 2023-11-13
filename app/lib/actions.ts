667d1c35d21e265fd3db75153b8b6c5e4e4ae44b
Ivan S <xsmirx@gmail.com>
2023-11-13 15:34:28 +0400

    authantication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import {signIn} from '@/auth'
 import {sql} from '@vercel/postgres'
 import {revalidatePath} from 'next/cache'
 import {redirect} from 'next/navigation'
@@ -109,5 +110,16 @@ export async function deleteInvoice(id: string) {
     return {message: 'Deleted Invoice.'}
   } catch (error) {
     return {message: 'Database Error: Failed to Delete Invoice.'}
+  }
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData))
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialSignin'
+    }
+    throw error
   }
 }
