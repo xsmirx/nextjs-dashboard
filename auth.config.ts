f2e9db193cca30b26a83021b042e2538a6fa086e
Ivan S <xsmirx@gmail.com>
2023-11-13 20:10:45 +0400

    metadata

--- auth.config.ts
+++ auth.config.ts
@@ -7,9 +7,6 @@ export const authConfig: NextAuthConfig = {
   },
   callbacks: {
     authorized({auth, request: {nextUrl}}) {
-      console.log('auth', auth)
-      console.log('nextUrl', nextUrl)
-
       const isLoggedIn = !!auth?.user
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard')
       if (isOnDashboard) {
